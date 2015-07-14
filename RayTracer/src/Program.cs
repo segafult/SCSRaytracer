9f7307c0e356c0beb911ebcd6660effd1b325a10
Elanna Stephenson <elanna.stephenson@gmail.com>
2015-07-13 21:44:56 -0600

    Added license info at top of source files.

--- RayTracer/src/Program.cs
+++ RayTracer/src/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//    
+//    Copyright(C) 2015  Elanna Stephenson
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.If not, see<http://www.gnu.org/licenses/>.
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,7 +32,7 @@ namespace RayTracer
             w.build();
             w.camera.render_scene(w);
             w.drawPlan.RotateFlip(System.Drawing.RotateFlipType.RotateNoneFlipY);
-            w.drawPlan.Save("E:\\test.bmp");
+            //w.drawPlan.Save("E:\\test.bmp");
             //Console.ReadKey();
         }
     }
